c412cb0a0bdbb7186e1918898913cfdf9dcddb30
catalin25 <catalin.tudoreanu1@gmail.com>
2023-03-12 18:25:03 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact Component={Home}/>
+        <Route path='/sign-up' exact Component={SignUp}/>
+        <Route path='/products' exact Component={Products}/>
+        <Route path='/services' exact Component={Services}/>
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
